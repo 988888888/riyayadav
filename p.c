94d1382180d5964fa52aa576a3c65604c14a33db
988888888 <30743036+988888888@users.noreply.github.com>
2017-08-04 22:43:23 -0700

    Update p.c

--- p.c
+++ p.c
@@ -1,3 +1,4 @@
+#include<stdio.h>
 int main()
 {
 int num;
@@ -11,7 +12,8 @@ else if(num<0)
 printf("number is negative");
 }
 else
-printf("number is zero");
+{
+  printf("number is zero");
 }
 return 0;
 }
